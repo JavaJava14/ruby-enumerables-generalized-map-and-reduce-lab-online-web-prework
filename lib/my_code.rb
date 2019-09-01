1513d8d228bdf6c797a785f2d1dbdbea0678ec74
Jose Chavira <joeychavira2016@gmail.com>
2019-09-01 19:02:41 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,20 @@
-# Your Code Here
+#expect(map([1, 2, 3, -9]){|n| n * -1}).to eq([-1, -2, -3, 9])
+
+def map(source_array)
+  new = []
+  i = 0
+  while i < source_array.length do
+    new.push(yield(source_array[i]))
+    i += 1
+  end
+  new
+end
+
+def reduce(source_array,starting_point = 0)
+  new = starting_point
+  i = 0
+  while i < source_array.length do
+    new = yield(source_array[i])
+    i += 1
+  end
+end
