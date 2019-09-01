6b219ce5a213ab71809b6acf81c573b3af2eb8e1
Jose Chavira <joeychavira2016@gmail.com>
2019-09-01 19:53:01 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,5 +1,6 @@
 #expect(map([1, 2, 3, -9]){|n| n * -1}).to eq([-1, -2, -3, 9])
-
+expect(reduce(source_array){|memo, n| memo + n}).to eq(6)
+expect(reduce(source_array, starting_point){|memo, n| memo + n}).to eq(106)
 def map(source_array)
   new = []
   i = 0
@@ -11,10 +12,10 @@ def map(source_array)
 end
 
 def reduce(source_array,starting_point = 0)
-  new = starting_point
+  origin = starting_point
   i = 0
   while i < source_array.length do
-    new = yield(source_array[i])
+    origin = yield(origin,source_array[i])
     i += 1
   end
 end
