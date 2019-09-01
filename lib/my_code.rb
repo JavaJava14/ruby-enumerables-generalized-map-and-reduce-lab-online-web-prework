6a6813faba4a478c39ec8a697581a0c511fbd68e
Jose Chavira <joeychavira2016@gmail.com>
2019-09-01 20:03:24 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,6 +1,4 @@
-#expect(map([1, 2, 3, -9]){|n| n * -1}).to eq([-1, -2, -3, 9])
-expect(reduce(source_array){|memo, n| memo + n}).to eq(6)
-expect(reduce(source_array, starting_point){|memo, n| memo + n}).to eq(106)
+
 def map(source_array)
   new = []
   i = 0
@@ -11,17 +9,17 @@ def map(source_array)
   new
 end
 
-def reduce(s, sp=nil)
-  if sp
-    accum = sp
-    i = 0
-  else
-    accum = s[0]
-    i = 1
-  end
-  while i < s.length
-    accum = yield(accum, s[i])
-    i += 1
-  end
-  accum
+def reduce(source_array,starting_point = nil)
+  if starting_point
+  origin = starting_point
+  i = 0
+else
+  origin = source_array[0]
+  i = 1
+end
+while i < source_array.length
+  origin = yield(origin, source_array[i])
+  i += 1
+end
+origin
 end
